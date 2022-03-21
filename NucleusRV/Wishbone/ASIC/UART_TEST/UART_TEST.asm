e64f6d284c89ab8cfe662c685ea028d73b96b00f
Talha Ahmed <talhaahmedashraf@gmail.com>
2022-03-21 23:27:36 +0500

    ASIC UART test done

---
+++ NucleusRV/Wishbone/ASIC/UART_TEST/UART_TEST.asm
@@ -0,0 +1,16 @@
+li x3 0x40001000 #GPIO
+li s0 0x40002000 #UART
+li x5 0x40000000 #DCCM
+
+
+
+li x6 , 10 #buad_rate
+li x9 , 1
+li x10 , 0
+sw x6 , 0(s0) #configuring control register
+li x7 , 0x77
+sw x7 , 4(s0) #write data
+sw x9 , 16(s0) #tx_enable
+sw x10 , 16(s0) #tx_disable
+rep:
+jal rep
